8c5e64991627ae280c48c99295a7dce208cb98ea
Akenxkq <88241587+Akenxkq@users.noreply.github.com>
2021-08-12 20:54:47 +0800

    Update gua_opencard8.js

--- gua_opencard8.js
+++ gua_opencard8.js
@@ -59,7 +59,7 @@ message = ""
     return;
   }
   if ($.isNode()) {
-    if (!process.env.guaopencard8 || process.env.guaopencard8 == "false") {
+    if (!process.env.guaopencard8 || process.env.guaopencard8 == "true") {
       console.log('如需执行脚本请设置环境变量[guaopencard8]为"true"')
       return
     }
